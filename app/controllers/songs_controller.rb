8eec7d48b3647b2fb759d9d1be55712409cd8a25
Grace Shih <vocalise@gmail.com>
2019-01-11 06:15:21 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -8,6 +8,8 @@ class SongsController < ApplicationController
   
   def show
     @song = Song.find(params[:id])
+    @genre = Genre.find(@song.genre_id)
+    @artist = Artist.find(@song.artist_id)
   end
   
   def create
